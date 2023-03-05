b3d5de1fc078d07874c01cac00a771463a158068
Eileen Chen <ezch09@gmail.com>
2023-03-05 02:09:21 -0500

    remove large video file

--- src/components/Projects/What2Yum/assets/index.js
+++ src/components/Projects/What2Yum/assets/index.js
@@ -10,7 +10,6 @@ import LofiSyncSharingGif from './lofi-sync-sharing-gif.mov';
 import HifiSwipeChoices from './hifi-swipe-vs-choices-flow.png';
 import HifiSuggestionV1 from './hifi-suggestions-v1.png';
 import HifiSuggestionV2 from './hifi-suggestions-v2.png';
-import DemoVid from './what2yum-demo-vid.m4v';
 import AppLaunch from './final-vids/app-launch.mov';
 import Create from './final-vids/creating-what2yum.mov';
 import Join from './final-vids/joining-what2yum.mp4';
@@ -31,7 +30,6 @@ export {
   HifiSwipeChoices,
   HifiSuggestionV1,
   HifiSuggestionV2,
-  DemoVid,
   AppLaunch,
   Create,
   Join,
