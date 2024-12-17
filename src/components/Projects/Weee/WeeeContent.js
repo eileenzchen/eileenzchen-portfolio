a23fe71ba65509379cb1f5b7f5fd200d7356fe7d
Eileen Chen <ezch09@gmail.com>
2024-12-16 21:20:27 -0800

    update weee

--- src/components/Projects/Weee/WeeeContent.js
+++ src/components/Projects/Weee/WeeeContent.js
@@ -65,7 +65,7 @@ export function Inspiration() {
     <div id="Inspiration" className="project-section">
       <h1><span className="section-title highlight">inspiration</span></h1>
       <p>
-        <b>I grew up loving to grocery shop.</b> It was a ritual that I had with my mom to go to the 99 Ranch, the Asian supermarket, and Costco every weekend. We perused the aisles for the newest ice cream and fresh pastries, checked the store magazines for the best sales, and ran into neighbors and friends who told us about their good finds.
+        <b>I grew up loving to grocery shop.</b> Every weekend, my mom and I went to 99 Ranch (an Asian supermarket) and Costco. We perused the aisles for the newest ice cream and fresh pastries, checked the store magazines for the best sales, and ran into neighbors and friends who told us about their good finds.
       </p>
       <p>
         Now with the prevalence of online grocery shopping, I <b>wanted to bring this sense of wonder and community</b> to a platform myself, my family, and friends commonly use to deliver groceries&mdash;Weee!.
